158789488c469bc0a864b7d9ec1ec1fab3ebb811
Your Name <franklin.cfg@gmail.com>
2022-09-14 10:48:19 -0400

    add express server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+//(**NOTE hide environment variable in .env file) require("dotenv").config()
+
+
+
+// ***Old method const express = require("express")*****
+
+
+//express app
+const app = express()
+
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//Routes
+app.get("/", (req, res)=> {
+    res.json({msg: "Welcome to KidzSnap Backend Database Support"});
+})
+
+//listen for requests
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
+
