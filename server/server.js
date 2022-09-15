78b7a468561ebe3bff0ce7e0964541138e131c54
Your Name <franklin.cfg@gmail.com>
2022-09-15 03:51:26 -0400

    bug fix .env file not working

--- server/server.js
+++ server/server.js
@@ -4,19 +4,29 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
+// import { createRequire } from 'module';
+
+
 //(**NOTE hide environment variable in .env file) require("dotenv").config()
+
+dotenv.config();
 
 
 
 // ***Old method const express = require("express")*****
 
 
+
+// const messageRoutes = require("./routes/messages")
+
+
 //express app
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 //middleware
+// app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
@@ -27,8 +37,12 @@ app.get("/", (req, res)=> {
     res.json({msg: "Welcome to KidzSnap Backend Database Support"});
 })
 
+// app.use("/api/messages", messageRoutes)
+
 //listen for requests *make change to .env file*
 app.listen(3000, () => {
-    console.log("listening on port 3000")
+    console.log("listening on port " , process.env.PORT)
 })
+
+
 
