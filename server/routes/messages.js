06f9ee8674684d3985d9677adfbd41a90b7ba716
asad2069 <asad2069@gmail.com>
2022-09-20 14:05:08 -0400

    Test 3

---
+++ server/routes/messages.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {createMessage, getMessage, getMessages, deleteMessage, updateMessage} from "../controllers/messageController";
+
+const router = express.Router()
+
+import PostMessage from "../models/messageModel"
+
+// GET all messages
+router.get("/", getMessages)
+
+// GET a single message
+router.get("/:id", getMessage)
+
+
+// POST a new message
+    router.post("/", createMessage)
+    
+    
+// DELETE a message
+router.delete("/:id", deleteMessage)
+
+
+// UPDATE a message
+router.patch("/:id", updateMessage)
+
+export default router
