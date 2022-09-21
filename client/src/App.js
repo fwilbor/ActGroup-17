76fdee52b7c3f6061c4aa9a0241f2e3b84682efc
Your Name <franklin.cfg@gmail.com>
2022-09-21 19:55:37 -0400

    update workingfrontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+
+
 // pages & components
 
 import Home from "./pages/Home"
 import  Navbar from "./components/Navbar"
+
 function App() {
   return (
     <div className="App">
