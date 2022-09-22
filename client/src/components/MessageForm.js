b278315f0b249f29ba4b9ce70ebd264d5836f141
Your Name <franklin.cfg@gmail.com>
2022-09-22 17:46:15 -0400

    will remove node modules

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,80 +1,68 @@
-import { useState } from "react"
+import { useState } from 'react'
+
 const MessageForm = () => {
-    const [title, setTitle] = useState("") 
-    const [message, setMessage] = useState("") 
-    const [creator, setCreator] = useState("")
-    const [error, setError] = useState(null) 
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('')
+  const [creator, setCreator] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+    // eslint-disable-next-line
+    var message = {title, message, creator}
+    
+    const response = await fetch('/api/messages', {
+      method: 'POST',
+      body: JSON.stringify(message),
+      headers: {
+        'Content-Type': 'application/json'
+     }
+    })
+    const json = await response.json()
 
-        // eslint-disable-next-line
-        const message = {title, message, creator}
-
-        const response = await fetch("/api/messages", {
-            method: "POST",
-            body: JSON.stringify(message),
-            headers: {
-                "Content-Type": "application/json"
-            }
-
-
-
-        })
-
-        const json = await response.json()
-
-        if (!response.ok) {
-            setError(json.error)
-        }
-
-        if (response.ok) {
-            setTitle("")
-            setMessage("")
-            setCreator("")
-            setError(null)
-            console.log("new message added", json)
-        }
-
-
-
+    if (!response.ok) {
+      setError(json.error)
     }
-    
-    
-    
-    
-    
-    
-    return (
-       <form className="create" onSubmit={handleSubmit}>
-        <h3>Add a New Message</h3>
+    if (response.ok) {
+      setError(null)
+      setTitle('')
+      setMessage('')
+      setCreator('')
+      console.log('new message added:', json)
+    }
 
-        <label>Message Title: </label>
-        <input
-        type="text"
-        onChange={(e) => setTitle(e.target.value)}
+  }
+
+  return (
+    <form className="create" onSubmit={handleSubmit}> 
+      <h3>Add a New Message</h3>
+
+      <label>Title: </label>
+      <input 
+        type="text" 
+        onChange={(e) => setTitle(e.target.value)} 
         value={title}
-        />
+      />
 
-<label>Message: </label>
-        <input
-        type="text"
-        onChange={(e) => setMessage(e.target.value)}
+      <label>Message: </label>
+      <input 
+        type="text" 
+        onChange={(e) => setMessage(e.target.value)} 
         value={message}
-        />
+      />
 
-<label>Message Creator: </label>
-        <input
-        type="text"
-        onChange={(e) => setCreator(e.target.value)}
-        value={creator}
-        />
+      <label>Creator: </label>
+      <input 
+        type="text" 
+        onChange={(e) => setCreator(e.target.value)} 
+        value={creator} 
+      />
 
-        <button>Add Message</button>
-        {error && <div className="error">{error}</div>}
-
-       </form>
-    )
+      <button>Add Message</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  )
 }
 
 export default MessageForm
