4e1a168a38903e236939cbb2034d55ac08961cb4
Your Name <franklin.cfg@gmail.com>
2022-09-27 15:30:17 -0400

    working login perhaps

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -49,6 +49,31 @@ userSchema.statics.signup = async function ( email, password) {
     return user
 }
 
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+    
+    if (!email || !password) {
+        throw Error("All fields must be filled")
+    }
+
+    const user = await this.findOne({email})
+
+    if (!user) {
+        throw Error("Invalid Email")
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error("Invalid Password")
+    }
+
+    return user
+
+
+}
+
 var userModel = mongoose.model("User", userSchema);
 
 export default userModel
