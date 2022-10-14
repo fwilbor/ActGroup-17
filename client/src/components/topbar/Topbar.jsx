b1f621a49894c0a45e65b295225a9973a8306d55
Your Name <franklin.cfg@gmail.com>
2022-10-14 14:15:15 -0400

    add topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,26 +1,26 @@
-import "./topbar.css"
-import {Search, Person, Chat, Notifications} from "@material-ui/icons";
-
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-       <div className="topbarLeft">
-        <span className="logo">KidzSnap logo goes here</span>
-       </div>
-       <div className="topbarCenter">
-        <div className="searchbar"></div>
-        <Search />
-        <input placeholder="Seach for friend, post or Snapz" className="seachInut" />
-
-        
-       
-       </div>
-       
-       <div className="topbarRight">
+      <div className="topbarLeft">
+        <span className="logo">KidzSnap</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            className="searchInput"
+            placeholder="Search for friend, messages or images"
+            
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
         <div className="topbarLinks">
-            <span className="topbarLink">Homepage</span>
-            <span className="topbarLink">Dashboard</span>
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
         </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
@@ -33,11 +33,12 @@ export default function Topbar() {
           </div>
           <div className="topbarIconItem">
             <Notifications />
-            <span className="topbarIconBadge">3</span>
+            <span className="topbarIconBadge">1</span>
           </div>
         </div>
-        <img src="/assets/dullstretch5.jpeg" alt="Incredible1" className="topbarImg" />
-       </div>
+        <img src="/assets/dullstretch5.jpeg" alt="Incredible1" className="topbarImg"/>
+      </div>
     </div>
   );
 }
+
