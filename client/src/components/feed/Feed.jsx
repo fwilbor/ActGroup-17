f690db0de6e0659aa0f0b7756d96c3842a2042a9
Your Name <franklin.cfg@gmail.com>
2022-10-18 02:05:46 -0400

    update feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,11 +1,19 @@
 import "./feed.css";
 import Share from "../share/Share";
 
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 export default function Feed() {
+
+  const user = useAuthContext()
+  console.log(user)
+  
   return (
+    
     <div className="feed">
       <div className="feedWrapper">
       <Share />
+      
 
       </div>
     </div>
--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,5 +1,5 @@
 import "./share.css"
-import {Image} from "@material-ui/icons"
+import {Image, Chat, LocationOn} from "@material-ui/icons"
 
 export default function Share() {
   return (
@@ -14,7 +14,30 @@ export default function Share() {
             <div className="shareOptions">
                 <div className="shareOption">
                     <Image className ="shareIcon"/>
-                    <span className="shareOptionText">Images</span>
+                    <div className="snap">
+                         <span className="shareOptionText">Snapz</span>
+                         </div>
+                   
+
+                </div>
+                <div className="shareOption">
+                    <Chat className ="shareIcon"/>
+                    <div className="flags">
+                    <span className="shareOptionText">Messages</span>
+                    </div>
+                    
+
+                </div>
+                <div className="shareOption">
+                    <LocationOn className ="shareIcon"/>
+
+                    <div className="location">
+
+                    <span  className="shareOptionText">Location</span>
+                   
+                    </div>
+
+                   
 
                 </div>
             </div>
