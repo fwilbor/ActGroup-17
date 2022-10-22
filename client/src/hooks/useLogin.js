8f9bd47232668ff153f20c48ffb13b45d851f6de
Your Name <franklin.cfg@gmail.com>
2022-10-22 09:42:40 -0400

    useLogin hook

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -10,30 +10,37 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch("/api/user/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({ email, password })
-        })
 
-        const json = await response.json()
+        try {
 
-        if (!response.ok) {
-            setIsLoading(false)
-            setError(json.error)
+            const response = await fetch("/api/user/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({ email, password })
+            })
+            const json = await response.json()
+            if (!response.ok) {
+                setIsLoading(false)
+                setError(json.error)
+            }
+    
+            if (response.ok) {
+                // save the user to local storage via web token
+                localStorage.setItem("user", JSON.stringify(json))
+    
+                // update the auth context
+                dispatch({type: "LOGIN", payload: json})
+    
+                // setIsLoading is false
+                setIsLoading(false)
+    
+            }
+        } catch (error) {
+            console.log(error.message);
+
         }
 
-        if (response.ok) {
-            // save the user to local storage via web token
-            localStorage.setItem("user", JSON.stringify(json))
 
-            // update the auth context
-            dispatch({type: "LOGIN", payload: json})
-
-            // setIsLoading is false
-            setIsLoading(false)
-
-        }
     }
 
     return { login, isLoading, error }
