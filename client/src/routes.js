c16f23f98b46a43b51e98228bc9b7a427a6bd5a5
Jeffery <jpastvait@gmail.com>
2022-12-03 20:40:21 -0500

    committing code refactor, working

---
+++ client/src/routes.js
@@ -0,0 +1,84 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import SimpleLayout from './layouts/simple';
+//
+import UserPage from './pages/UserPage';
+import LoginPage from './pages/LoginPage';
+import Page404 from './pages/Page404';
+import DashboardAppPage from './pages/DashboardAppPage';
+
+//
+import SetAvatar from "./components/SetAvatar";
+//import { useAuthContext } from "./hooks/useAuthContext";
+
+
+// pages & components
+
+import FileUpload from "./components/FileUpload";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import  Navbar from "./components/Navbar";
+import Chat from "./pages/Messenger";
+//
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: 'app', element: <DashboardAppPage /> },
+        { path: 'user', element: <UserPage /> },
+      ],
+    },
+    {
+      path: 'login',
+      element: <LoginPage />,
+    },
+    {
+      element: <SimpleLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: '404', element: <Page404 /> },
+        { path: '*', element: <Navigate to="/404" /> },
+      ],
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
+    {
+      path: "/setAvatar",
+      // element={!user ? <Navigate to = "/" />: <Messenger /> }
+      element: <SetAvatar />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+    {
+      path: "/uploads",
+      element: <FileUpload />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/messenger",
+      // element={!user ? <Navigate to = "/" />: <Messenger /> }
+      element: <Chat />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ]);
+
+  return routes;
+}
