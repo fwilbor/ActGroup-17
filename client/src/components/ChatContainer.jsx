688b9b6f31582040eb3610e8bf7225b1b30efbcb
jcassiday <mjcassiday@gmail.com>
2022-12-08 13:32:51 -0500

    Update Chat Container

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -5,11 +5,7 @@ import Logout from "./Logout";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { sendMessageRoute, recieveMessageRoute } from "../utils/APIRoutes";
-<<<<<<< HEAD
-//import SwearWordCheck from "./SwearWordCheck";
-=======
 import SwearWordCheck from "../components/SwearWordCheck.jsx"
->>>>>>> 436098e4924ef29c75cb0dc20cbb482aeedd37cd
 
 export default function ChatContainer({ currentChat, socket }) {
   const [messages, setMessages] = useState([]);
@@ -112,13 +108,8 @@ export default function ChatContainer({ currentChat, socket }) {
                   message.fromSelf ? "sended" : "recieved"
                 }`}
               >
-<<<<<<< HEAD
-                <div className="content " >
-                  <p>{message.message}</p>
-=======
                 <div className="content ">
                   <p style={ flag === "Yes" ? { background: '#f06f6f', border: '1px solid red'} : {}}>{message.message}</p>
->>>>>>> 436098e4924ef29c75cb0dc20cbb482aeedd37cd
                 </div>
               </div>
             </div>
