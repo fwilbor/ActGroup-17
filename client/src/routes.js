09f7ff22b3481ae6e3770b9cdc576697911166c1
Your Name <franklin.cfg@gmail.com>
2022-12-08 17:54:01 -0500

    delete eslint file for airbnb fix

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,7 +4,7 @@ import DashboardLayout from './layouts/dashboard';
 import SimpleLayout from './layouts/simple';
 //
 import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
+
 import Page404 from './pages/Page404';
 import DashboardAppPage from './pages/DashboardAppPage';
 
@@ -19,7 +19,6 @@ import FileUpload from "./components/FileUpload";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import  Navbar from "./components/Navbar";
 import Chat from "./pages/Messenger";
 //
 
@@ -36,10 +35,7 @@ export default function Router() {
         { path: 'user', element: <UserPage /> },
       ],
     },
-    {
-      path: 'login',
-      element: <LoginPage />,
-    },
+    
     {
       element: <SimpleLayout />,
       children: [
