15f3b3cbca048c0773b8ac00536756168ebf26e3
jcassiday <mjcassiday@gmail.com>
2023-03-17 15:42:09 -0400

    Testing github connection

--- server/server.js
+++ server/server.js
@@ -38,7 +38,6 @@ const CONNECTION_URL = process.env.MONGO_URI
 
 
 const PORT = process.env.PORT
-//const APP_PORT = process.env.APP_PORT;
 
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
