8a15852cfdbc7230e52cb39c922f3cce658be9b3
jcassiday <mjcassiday@gmail.com>
2023-03-17 14:10:07 -0400

    Working avatarImage in icon

--- client/src/layouts/dashboard/header/AccountPopover.js
+++ client/src/layouts/dashboard/header/AccountPopover.js
@@ -6,7 +6,28 @@ import { useNavigate } from "react-router-dom";
 import Logout from "../../../components/Logout.js"
 import axios from "axios";
 
+const base64string = "PD94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/Pgo8IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDIwMDEwOTA0Ly9FTiIKICJodHRwOi8vd3d3LnczLm9yZy9UUi8yMDAxL1JFQy1TVkctMjAwMTA5MDQvRFREL3N2ZzEwLmR0ZCI+CjxzdmcgdmVyc2lvbj0iMS4wIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciCiB3aWR0aD0iMTAwLjAwMDAwMHB0IiBoZWlnaHQ9IjEwMC4wMDAwMDBwdCIgdmlld0JveD0iMCAwIDEwMC4wMDAwMDAgMTAwLjAwMDAwMCIKIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaWRZTWlkIG1lZXQiPgoKPGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoMC4wMDAwMDAsMTAwLjAwMDAwMCkgc2NhbGUoMC4xMDAwMDAsLTAuMTAwMDAwKSIKZmlsbD0iIzAwMDAwMCIgc3Ryb2tlPSJub25lIj4KPHBhdGggZD0iTTQ2MCA4NzggYy0xMDcgLTMwIC0xNTIgLTE2NyAtODMgLTI0OSA3NSAtODkgMjExIC03OCAyNjUgMjEgMjUgNDUKMjAgMTI5IC05IDE2OCAtNDAgNTMgLTExMSA3OCAtMTczIDYweiBtMTAxIC01OCBjMjQgLTEzIDQ5IC02MSA0OSAtOTQgMCAtNjIKLTQ2IC0xMDYgLTExMyAtMTA2IC05OCAwIC0xMzcgMTM3IC01NiAxOTQgMjYgMTggOTAgMjEgMTIwIDZ6Ii8+CjxwYXRoIGQ9Ik0zOTMgNTExIGMtMTE3IC00MCAtMTc4IC0xMTYgLTE5OCAtMjQ3IC02IC00MSAtNiAtNDEgNDQgLTc0IDE1MwotOTggMzcwIC05OCA1MjMgLTEgbDQ3IDMwIC00IDU1IGMtMTEgMTgzIC0yMjAgMzA0IC00MTIgMjM3eiBtMjMyIC01OCBjNzAKLTM2IDExNSAtOTEgMTI5IC0xNTkgNiAtMjggMyAtMzUgLTI1IC01NSAtMTI1IC04OSAtMzIxIC05MyAtNDM3IC05IC00OCAzNAotNTEgNTMgLTIzIDExNiAyMiA0OCA3OSA5OCAxMzYgMTE5IDU2IDIwIDE2OCAxNCAyMjAgLTEyeiIvPgo8L2c+Cjwvc3ZnPgo="
+
 // ----------------------------------------------------------------------
+
+export default function AccountPopover() {
+  const [open, setOpen] = useState(null);
+  const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState("");
+
+useEffect(() => {
+  const fetchData=async()=>{
+  if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+    navigate("/login");
+  } else {
+    const user = await JSON.parse(
+      localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+    );
+    setCurrentUser(user);
+  }
+};
+fetchData();
+}, [navigate, setCurrentUser]);
 
 const MENU_OPTIONS = [
   {
@@ -32,28 +53,6 @@ const MENU_OPTIONS = [
 ];
 
 // ----------------------------------------------------------------------
-
-
-export default function AccountPopover() {
-  const [open, setOpen] = useState(null);
-  const navigate = useNavigate();
-  const [currentUser, setCurrentUser] = useState("");
-
-useEffect(() => {
-  const fetchData=async()=>{
-  if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
-    navigate("/login");
-  } else {
-    setCurrentUser(
-      await JSON.parse(
-        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
-      )
-    );
-    
-  }
-}
-fetchData();
-}, [navigate]);
 
   const handleOpen = (event) => {
     setOpen(event.currentTarget);
@@ -82,7 +81,9 @@ fetchData();
           }),
         }}
       >
-        <Avatar src={currentUser.avatarImage} alt={currentUser.username ? currentUser.username.charAt(0) : ''} />
+        <Avatar 
+  src={`data:image/svg+xml;base64,${currentUser.avatarImage ? currentUser.avatarImage : base64string}`} 
+/>
       </IconButton>
 
       <Popover
