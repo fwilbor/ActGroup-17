6ae867ddea4a80a2dfd008b6698b6a10ba93527b
jcassiday <mjcassiday@gmail.com>
2023-06-06 21:23:55 -0400

    Modify routes so user returns to their home page if they mispell in url, add useEffect to settings to keep children out

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,60 +1,55 @@
-import { Navigate, useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes, useNavigate } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import SimpleLayout from './layouts/simple';
-//
+// pages & components
 import UserPage from './pages/UserPage';
 import Page404 from './pages/Page404';
 import DashboardAppPage from './pages/DashboardAppPage';
-
-//
-//import SetAvatar from "./components/SetAvatar";
-//import { useAuthContext } from "./hooks/useAuthContext";
-
-
-// pages & components
-
-import FileUpload from "./components/FileUpload";
 import CreateChild from "./pages/CreateChild";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/ChatRoom";
-import Messenger from "./pages/Messenger";
 import Settings from "./pages/Settings"
 import AddFriend from "./pages/AddFriend"
 import SetAvatar from "./pages/SetAvatarPage";
-import WebcamCapture from './components/WebcamCapture';
 import Splash from './components/Splash';
 //
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
+  const adult_or_child = JSON.parse(
+    localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+  );
+
+  const isParent = adult_or_child && !adult_or_child.parentLink;
+  const isChild = adult_or_child && adult_or_child.parentLink;
+
+  const navigate = useNavigate();
+
   const routes = useRoutes([
     {
       path: '/dashboard',
-      element: <DashboardLayout />,
+      element: isParent ? <DashboardLayout /> : <Navigate to="/chat" />,
       children: [
         { element: <Navigate to="/dashboard/app" />, index: true },
         { path: 'app', element: <DashboardAppPage /> },
-        { path: 'user', element: <UserPage /> },
       ],
     },
-
     {
       path: '*',
-      element: <Navigate to="/splash" replace />,
+      element: isParent ? <Navigate to="/dashboard" /> : <Navigate to="/chat" />,
     },
     {
-      path: "/chat",
-      // element={!user ? <Navigate to = "/" />: <Messenger /> }
-      element: <Chat />,
+      path: '/chat',
+      element: isChild ? <Chat /> : <Navigate to="/splash" />,
     },
-    {
-      path: "/messenger",
-      // element={!user ? <Navigate to = "/" />: <Messenger /> }
-      element: <Messenger />,
-    },
+    // {
+    //   path: "/messenger",
+    //   // element={!user ? <Navigate to = "/" />: <Messenger /> }
+    //   element: <Messenger />,
+    // },
     {
       path: "/addfriend",
       element: <AddFriend />,
@@ -72,14 +67,14 @@ export default function Router() {
       path: "/settings",
       element: <Settings />,
     },
-    {
-      path: "/uploads",
-      element: <FileUpload />,
-    },
-    {
-      path: "/addImage",
-      element: <WebcamCapture />,
-    },
+    // {
+    //   path: "/uploads",
+    //   element: <FileUpload />,
+    // },
+    // {
+    //   path: "/addImage",
+    //   element: <WebcamCapture />,
+    // },
     {
       path: "/login",
       element: <Login />,
