e3d3c8c9fbade71a8bcfbd98f01334f6cda34d97
jcassiday <mjcassiday@gmail.com>
2023-06-24 13:35:26 -0400

    Update socket to work on live site

--- server/chat.js
+++ server/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://www.kidzsnap.com');
 
 // Query DOM
 var message = document.getElementById('message'),
